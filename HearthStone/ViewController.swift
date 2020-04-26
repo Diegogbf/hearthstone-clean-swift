7309540faf724b1e52ea6ba9f08722a6bcb395a1
Diego Gomes <diego.gomes@mobile2you.com.br>
2020-04-25 21:37:25 -0300

    Initial Commit

---
+++ HearthStone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HearthStone
+//
+//  Created by Diego Gomes on 25/04/20.
+//  Copyright © 2020 Diego Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HearthStoneTests/HearthStoneTests.swift
@@ -0,0 +1,34 @@
+//
+//  HearthStoneTests.swift
+//  HearthStoneTests
+//
+//  Created by Diego Gomes on 25/04/20.
+//  Copyright © 2020 Diego Gomes. All rights reserved.
+//
+
+import XCTest
+@testable import HearthStone
+
+class HearthStoneTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
